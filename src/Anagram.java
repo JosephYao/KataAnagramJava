057efbfffa99e9e5958e45dd2195cce2b0b531c6
josephyao <dermott@126.com>
2015-05-19 20:30:34 +0800

    one character word list and source is double of this character will have one anagram

--- src/Anagram.java
+++ src/Anagram.java
@@ -2,10 +2,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Anagram {
+    private final List<String> words;
+
     public Anagram(List<String> words) {
+        this.words = words;
     }
 
     public List<String> generate(String source) {
+        if (!this.words.isEmpty())
+            return Arrays.asList(words.get(0) + " " + words.get(0));
+
         return Arrays.asList();
     }
 }
--- src/TestAnagram.java
+++ src/TestAnagram.java
@@ -3,15 +3,23 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 
 public class TestAnagram {
 
     @Test
     public void empty_word_list_and_source_is_any_word_will_have_no_anagram() {
-        Anagram generator = new Anagram(Arrays.asList());
-        List<String> anagrams = generator.generate("anyword");
-        assertThat(anagrams, equalTo(Arrays.asList()));
+        assertThat(anagramsOf(Arrays.asList(), "anyword"), empty());
+    }
+
+    @Test
+    public void one_character_word_list_and_source_is_double_of_this_character_will_have_one_anagram() {
+        assertThat(anagramsOf(Arrays.asList("a"), "aa"), contains("a a"));
+    }
+
+    private List<String> anagramsOf(List<String> words, String source) {
+        return new Anagram(words).generate(source);
     }
 }
